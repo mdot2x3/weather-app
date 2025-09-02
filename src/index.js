d50ca3b2ae6e1111ea42d5b127e8521f9b380c99
mdot2x3 <mdot2x3@gmail.com>
2025-09-02 16:52:41 -0700

    feat: display content via the DOM
    add innerHTML to the forecast class in the HTML
    via DOM injection inside apiCall() in index.js.
    
    modify processData() in index.js to pull icon and
    temperature information from the JSON as well.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,65 @@ async function apiCall(userInput, degreeUnit) {
     );
     const responseData = await response.json();
     const finalData = await processData(responseData);
+    // view JSON object in console
     console.log(finalData);
+
+    // display content on the page via the DOM
+    const forecast = document.querySelector(".forecast");
+
+    forecast.innerHTML = `
+    <div class="current">
+        <p>"Today, the forecast for ${finalData.address} is ..."</p>
+        <p>${finalData.currentConditions.icon}</p>
+        <p>${finalData.currentConditions.temp}&deg</p>
+        <p>${finalData.currentConditions.conditions}</p>
+        <p>${finalData.currentDescription}</p>
+    </div>
+    
+    <div class="future">
+        <table>
+            <thead>
+                <tr>
+                    <th>${finalData.forecastDay1.datetime}</th>
+                    <th>${finalData.forecastDay2.datetime}</th>
+                    <th>${finalData.forecastDay3.datetime}</th>
+                    <th>${finalData.forecastDay4.datetime}</th>
+                    <th>${finalData.forecastDay5.datetime}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${finalData.forecastDay1.icon}</td>
+                    <td>${finalData.forecastDay2.icon}</td>
+                    <td>${finalData.forecastDay3.icon}</td>
+                    <td>${finalData.forecastDay4.icon}</td>
+                    <td>${finalData.forecastDay5.icon}</td>
+                </tr>
+                <tr>
+                    <td>${finalData.forecastDay1.conditions}</td>
+                    <td>${finalData.forecastDay2.conditions}</td>
+                    <td>${finalData.forecastDay3.conditions}</td>
+                    <td>${finalData.forecastDay4.conditions}</td>
+                    <td>${finalData.forecastDay5.conditions}</td>
+                </tr>
+                <tr>
+                    <td>${finalData.forecastDay1.temp}&deg</td>
+                    <td>${finalData.forecastDay2.temp}&deg</td>
+                    <td>${finalData.forecastDay3.temp}&deg</td>
+                    <td>${finalData.forecastDay4.temp}&deg</td>
+                    <td>${finalData.forecastDay5.temp}&deg</td>
+                </tr>
+                <tr>
+                    <td>${finalData.forecastDay1.description}</td>
+                    <td>${finalData.forecastDay2.description}</td>
+                    <td>${finalData.forecastDay3.description}</td>
+                    <td>${finalData.forecastDay4.description}</td>
+                    <td>${finalData.forecastDay5.description}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+`;
   } catch {
     (error) => {
       console.log(error);
@@ -19,7 +77,7 @@ async function apiCall(userInput, degreeUnit) {
   }
 }
 
-// function to filter .json data to desired items
+// function to filter JSON data to desired items
 function processData(data) {
   return {
     address: data.address,
@@ -27,31 +85,43 @@ function processData(data) {
     currentDescription: data.description,
     forecastDay0: {
       datetime: data.days[0].datetime,
+      icon: data.days[0].icon,
+      temp: data.days[0].temp,
       conditions: data.days[0].conditions,
       description: data.days[0].description,
     },
     forecastDay1: {
       datetime: data.days[1].datetime,
+      icon: data.days[1].icon,
+      temp: data.days[1].temp,
       conditions: data.days[1].conditions,
       description: data.days[1].description,
     },
     forecastDay2: {
       datetime: data.days[2].datetime,
+      icon: data.days[2].icon,
+      temp: data.days[2].temp,
       conditions: data.days[2].conditions,
       description: data.days[2].description,
     },
     forecastDay3: {
       datetime: data.days[3].datetime,
+      icon: data.days[3].icon,
+      temp: data.days[3].temp,
       conditions: data.days[3].conditions,
       description: data.days[3].description,
     },
     forecastDay4: {
       datetime: data.days[4].datetime,
+      icon: data.days[4].icon,
+      temp: data.days[4].temp,
       conditions: data.days[4].conditions,
       description: data.days[4].description,
     },
     forecastDay5: {
       datetime: data.days[5].datetime,
+      icon: data.days[5].icon,
+      temp: data.days[5].temp,
       conditions: data.days[5].conditions,
       description: data.days[5].description,
     },
